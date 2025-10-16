921979d359d6953f0dde3a3a63a4d0184e042646
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2025-10-16 13:44:33 +0100

    Python Basics

--- FirstProject/Demo.py
+++ FirstProject/Demo.py
@@ -1,1 +1,3 @@
 print('Hello World!')
+print("Hello \t World!")
+print("Hello \nWorld!")
--- FirstProject/SimpleCalc.py
+++ FirstProject/SimpleCalc.py
@@ -2,7 +2,6 @@ a=10
 b=20
 print('The sum is: ', a + b)
 
-
 #Dynamically Typed Programming Language
 c,d=100,200
 print('The sum is: ', c + d)
