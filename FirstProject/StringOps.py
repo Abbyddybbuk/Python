672dc5af8a9fd4a99e2068e2e299d52c57df4725
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2025-10-17 12:04:42 +0100

    String Ops

--- FirstProject/StringOps.py
+++ FirstProject/StringOps.py
@@ -5,3 +5,7 @@ print(myString[5])
 
 print(myString[:9]) #Output String upto index 9
 print(myString[2:]) #Output String from index 2 to end
+
+print(myString[::])# Output the whole string
+print(myString[::2]) #Output every second character
+print(myString[::-1]) #Output the string in reverse order
