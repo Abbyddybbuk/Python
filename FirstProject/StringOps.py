f9f4c201d5d0fd79aa9269a857d9771e3961531a
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2025-10-17 11:59:35 +0100

    String Ops

---
+++ FirstProject/StringOps.py
@@ -0,0 +1,7 @@
+myString = "Hello, World!"
+
+print(myString[-3])            # Output the string
+print(myString[5])  
+
+print(myString[:9]) #Output String upto index 9
+print(myString[2:]) #Output String from index 2 to end
