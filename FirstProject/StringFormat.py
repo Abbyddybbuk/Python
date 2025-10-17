b3d3ed7611c195471e1b55306e444bb589c5037a
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2025-10-17 15:01:43 +0100

    Formatting Floating Numbers

---
+++ FirstProject/StringFormat.py
@@ -0,0 +1,9 @@
+print("This is a {} string".format("test"))
+
+print("My {} is {} {}".format("SUV", "Blue", "Car"))
+
+print("My {a} is a {b} {c}".format(c="SUV", b="Blue", a="Car"))
+
+result = 100/666
+print("The result is {}".format(result))
+print("The result is {r:1.2f}".format(r=result))#formatted to show 2 decimal places with width of 1(spaces before number if needed)
