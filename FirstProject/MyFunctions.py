48898beded661f089cad44130f26d257387e9976
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2025-10-28 12:34:33 +0000

    Python Functions

---
+++ FirstProject/MyFunctions.py
@@ -0,0 +1,14 @@
+  
+
+def print_msg(msg):
+    print(f"Message: {msg}")
+
+print_msg("Hello from MyFunctions.py")  
+
+def add_numbers(a, b):
+    return a + b    
+
+
+result = add_numbers(5, 10)
+print(f"The sum of 5 and 10 is: {result}")
+
