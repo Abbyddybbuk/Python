c9aa79f62cc1e6d6b3ba91559ac9028038e4e1bd
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2025-10-28 12:53:23 +0000

    Python Functions

--- FirstProject/MyFunctions.py
+++ FirstProject/MyFunctions.py
@@ -18,3 +18,10 @@ def sum_numbers(num1, num2):
     return num1 + num2  
 
 print(f"Sum of 7 and 3 is: {sum_numbers('7', '3')}")#string concatenation due to string inputs
+
+
+def is_even(num):
+    return num % 2 == 0
+
+print(f"Is 4 even? {is_even(4)}")
+print(f"Is 7 even? {is_even(7)}")    
