5125d3c822e210d5e4cbe0c5982d8b07cd0bf142
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2026-03-11 23:15:41 +0000

    AI+Python Revision

--- FirstProject/FunctionsPython.py
+++ FirstProject/FunctionsPython.py
@@ -2,4 +2,8 @@ def print_full_name():
     print('My name is Abhijeet')
     print('My last name is Kulshreshtha')
 
+def print_greeting(name):    
+    print(f"Hello, {name}!")
+
 print_full_name()    
+print_greeting("Abhijeet")
